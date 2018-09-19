795cf73f80a28d35340db89d8734547b4ba7daff
broberts-at-abs-group <broberts@abs-group.com>
2018-09-19 14:56:04 -0400

    git fix

--- pemex_app/urls.py
+++ pemex_app/urls.py
@@ -7,15 +7,14 @@ urlpatterns = [
         #url(r'^add_evidence/$', views.add_evidence, name='add_evidence'),
 
         url(r'^upload/$', views.model_form_upload, name='upload'),
-        url(r'^fileretrieve/$', views.file_retrieve, name='fileretrieve')
+        url(r'^fileretrieve/$', views.file_retrieve, name='fileretrieve'),
         url(r'^assign/$', views.assign, name='assign'),
-        url(r'^inputs/$', views.inputs, name='inputs'),
 
 
-        url(r'^q_all/$', views.queue, {'prefilter' : 'none'}, name='q_all'),
-        url(r'^q_user/$', views.queue, {'prefilter' : 'user'}, name='q_user'),
-        url(r'^q_team/$', views.queue, {'prefilter' : 'users team group'}, name='q_team'),
-        url(r'^q_rev/$', views.queue, {'prefilter' : 'reviewer'}, name='q_rev'),
+        url(r'^q_all/$', views.queue_all, {'prefilter' : 'none'}, name='q_all'),
+        url(r'^q_user/$', views.queue_all, {'prefilter' : 'user'}, name='q_user'),
+        url(r'^q_team/$', views.queue_all, {'prefilter' : 'users team group'}, name='q_team'),
+        url(r'^q_rev/$', views.queue_all, {'prefilter' : 'reviewer'}, name='q_rev'),
         url(r'^q_trans/$', views.queue_translator, name='q_trans'),
 
         url(r'^update/(?P<pk>\d+)/$', views.compliance_update, name='compliance_update'),
